7f7a8fd9fa7a4cf3b8489e1f9922b08a099a1638
slbilona <ilonaselbonne@hotmail.fr>
2025-11-10 16:48:08 +0100

    redeploiement

--- front/pages/Accueil.jsx
+++ front/pages/Accueil.jsx
@@ -9,7 +9,7 @@ function Accueil() {
             <p className="text-xl md:text-1xl max-w-170 my-3">
                 Actuellement étudiante à l'école 42. Je suis à la recherche d'un stage en data/ia ou en développement full stack
             </p>
-            <button className="bg-trois/50 p-3 rounded-full text-white hover:bg-trois/60 transition duration-150"><a href="#Projets">Voir mes projets</a></button>
+            <a className="bg-trois/50 p-3 rounded-full text-white hover:bg-trois/60 transition duration-150" href="#Projets">Voir mes projets</a>
         </div>
     )
 }
