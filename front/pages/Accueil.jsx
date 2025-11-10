faaeb954559116e3b5bd998c1fefc5a2042f7643
slbilona <ilonaselbonne@hotmail.fr>
2025-11-10 17:11:08 +0100

    redeploiement

--- front/pages/Accueil.jsx
+++ front/pages/Accueil.jsx
@@ -1,7 +1,7 @@
 function Accueil() {
     return (
         <div id="Accueil" className="h-screen flex flex-col justify-center text-center items-center p-5 md:p-0">
-            <img src="../img/thumbnail_IMG_8407.jpg" className="w-70 h-70 object-cover md:w-80 md:h-80 md:object-cover mb-3 border-6 border-trois shadow-lg" style={{borderRadius : "21% 79% 70% 30% / 30% 31% 69% 70% "}}></img>
+            <img src="..\img\thumbnail_IMG_8407.jpg" className="w-70 h-70 object-cover md:w-80 md:h-80 md:object-cover mb-3 border-6 border-trois shadow-lg" style={{borderRadius : "21% 79% 70% 30% / 30% 31% 69% 70% "}}></img>
             <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-2">
                 <h1 className="font-bold text-4xl md:text-5xl">Bonjour, je suis</h1>
                 <h1 className="font-bold text-4xl md:text-5xl text-quatre">Ilona Selbonne</h1>
