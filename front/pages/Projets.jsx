6f1ca944f994dfedf593b6dd1fd6940c08f247fd
slbilona <ilonaselbonne@hotmail.fr>
2025-11-13 15:16:38 +0100

    update projets

--- front/pages/Projets.jsx
+++ front/pages/Projets.jsx
@@ -3,9 +3,9 @@ import ProjetsCard from "../components/ProjetsCards"
 
 function Projets() {
     const projets = [
-        {id:1, titre:"Portfolio", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.", github:"https://github.com/slbilona/Portfolio", skills:["React", "JavaScript", "Node.js", "Tailwind CSS"]},
-        {id:2, titre:"Ft_transcendance", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.", github:"https://github.com/slbilona/ft_transcendance_3.0", skills:["HTML", "CSS", "Django", "JavaScript", "Bootstrap"]},
-        {id:3, titre:"Ft_irc", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.", github:"https://github.com/slbilona/ft_transcendance_3.0", skills:["C++"]},
+        {id:1, titre:"Portfolio", description:"Mon portfolio présente mes réalisations en développement web, mes projets personnels et met en avant mes compétences.", github:"https://github.com/slbilona/Portfolio", skills:["React", "JavaScript", "Node.js", "Tailwind CSS"]},
+        {id:2, titre:"Ft_transcendance", description:"Un jeu Pong multijoueur en ligne avec gestion de profils, ajout/suppression d’amis, historique des parties et chat en direct via WebSockets.", github:"https://github.com/slbilona/ft_transcendance", skills:["HTML", "CSS", "Django", "JavaScript", "Bootstrap"]},
+        {id:3, titre:"Ft_irc", description:"Un serveur IRC conforme aux standards, permettant la communication en temps réel entre plusieurs clients.", github:"https://github.com/slbilona/ft_irc", skills:["C++"]},
     ]
 
     return (
