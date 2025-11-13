458eea2cb83a1a2f4a4e97f533a15a07c7b012dc
slbilona <ilonaselbonne@hotmail.fr>
2025-11-13 15:18:56 +0100

    update projets

--- front/pages/Projets.jsx
+++ front/pages/Projets.jsx
@@ -4,7 +4,7 @@ import ProjetsCard from "../components/ProjetsCards"
 function Projets() {
     const projets = [
         {id:1, titre:"Portfolio", description:"Mon portfolio présente mes réalisations en développement web, mes projets personnels et met en avant mes compétences.", github:"https://github.com/slbilona/Portfolio", skills:["React", "JavaScript", "Node.js", "Tailwind CSS"]},
-        {id:2, titre:"Ft_transcendance", description:"Un jeu Pong multijoueur en ligne avec gestion de profils, ajout/suppression d’amis, historique des parties et chat en direct via WebSockets.", github:"https://github.com/slbilona/ft_transcendance", skills:["HTML", "CSS", "Django", "JavaScript", "Bootstrap"]},
+        {id:2, titre:"Ft_transcendance", description:"Un jeu Pong multijoueur en ligne avec gestion de profils, ajout/suppression d’amis, historique des parties et chat en direct.", github:"https://github.com/slbilona/ft_transcendance", skills:["HTML", "CSS", "Django", "JavaScript", "Bootstrap"]},
         {id:3, titre:"Ft_irc", description:"Un serveur IRC conforme aux standards, permettant la communication en temps réel entre plusieurs clients.", github:"https://github.com/slbilona/ft_irc", skills:["C++"]},
     ]
 
